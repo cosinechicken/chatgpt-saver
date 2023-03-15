41707bb550f143a55b4f234bf5bebd0eb41f3a8d
Brandon <ah.blenden@gmail.com>
2023-03-15 13:57:37 -0400

    Added error messages

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function handleKeyDown(event) {
     event.preventDefault();
     const inputText = document.getElementById('input-text').value.trim();
     if (inputText.length > 0) {
-      updateOutput('You: ' + inputText);
+      updateOutput('You: ' + inputText, "text");
       document.getElementById('input-text').value = '';
       scrollToBottom();
       messages.push({"role": "user", "content": inputText})
@@ -29,25 +29,29 @@ function handleKeyDown(event) {
         .then(response => response.json())
         .then(data => {
           const outputText = data.choices[0]['message']['content'].trim();
-          updateOutput('ChatGPT: ' + outputText);
           messages.push({"role": "assistant", "content": outputText});
           scrollToBottom();
         })
         .catch((error) => {
           console.error('Error:', error);
+          updateOutput('Error: ' + error, "error");
         });
     }
   }
 }
 
-function updateOutput(text) {
+function updateOutput(text, type) {
     const outputPane = document.getElementById('output-pane');
     const messageElement = document.createElement('div');
     messageElement.classList.add('output-message');
-    if (text.startsWith('You:')) {
-      messageElement.classList.add('user-message');
-    } else if (text.startsWith('ChatGPT:')) {
-      messageElement.classList.add('assistant-message');
+    if (type === "text") {
+        if (text.startsWith('You:')) {
+            messageElement.classList.add('user-message');
+        } else if (text.startsWith('ChatGPT:')) {
+            messageElement.classList.add('assistant-message');
+        }
+    } else {
+        messageElement.classList.add('error-message');
     }
     const converter = new showdown.Converter();
     const html = converter.makeHtml(text);
