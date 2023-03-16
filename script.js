219b3987bf1cce17649745fed71d29af6bfa8c8f
Brandon <ah.blenden@gmail.com>
2023-03-16 12:50:01 -0400

    Test Cookie for APIKey

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let messages = [];
 let message_text = "";
-let apiKey = "";
+let apiKey = getCookie("apiKey");
 
 function handleKeyDown(event) {
   if (event.keyCode === 13 && !event.shiftKey) {
@@ -127,5 +127,31 @@ apicodetextarea.addEventListener("keydown", function (e) {
 
 function submitAPICode() {
     apiKey = apicodetextarea.value;
+    setCookie("apiKey", apiKey, 7);
     modal.style.display = "none";
 }
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    console.log("Cookie " + cname + " set to " + cvalue + " and will expire in " + exdays + ". ");
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        console.log("Cookie " + cname + ": " + c.substring(name.length, c.length));
+        return c.substring(name.length, c.length);
+      }
+    }
+    console.log("no cookie found");
+    return "";
+  }
